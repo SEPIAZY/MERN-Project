70815876affd5302f94d6781344719f4ac8f567a
Punnapa Thianchai <88400339+SEPIAZY@users.noreply.github.com>
2023-05-30 14:59:36 +0700

    master

---
+++ client/src/components/Register.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import avatar from '../assets/upload.jpg';
+import toast, { Toaster } from 'react-hot-toast';
+import { useFormik } from 'formik';
+import { registerValidation } from '../helper/validate';
+import convertToBase64 from '../helper/convert';
+import { registerUser } from '../helper/helper'
+
+
+import styles from '../styles/Profile.module.css';
+
+export default function Register() {
+
+  const navigate = useNavigate()
+  const [file, setFile] = useState()
+
+  const formik = useFormik({
+    initialValues : {
+      email: '',
+      username: '',
+      password : ''
+    },
+    validate : registerValidation,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit : async values => {
+      values = await Object.assign(values, { profile : file || ''})
+      let registerPromise = registerUser(values)
+      toast.promise(registerPromise, {
+        loading: 'Creating...',
+        success : <b>Register Successfully...!</b>,
+        error : <b>Could not Register.</b>
+      });
+
+      registerPromise.then(function(){ navigate('/')});
+    }
+  })
+
+  /** formik doensn't support file upload so we need to create this handler */
+  const onUpload = async e => {
+    const base64 = await convertToBase64(e.target.files[0]);
+    setFile(base64);
+  }
+
+  return (
+    <div className="container mx-auto">
+
+      <Toaster position='top-center' reverseOrder={false}></Toaster>
+
+      <div className='flex justify-center items-center h-screen'>
+        <div className={styles.glass}>
+
+          <div className="title flex flex-col items-center">
+            <h4 className={styles.content}>Sign up</h4>
+            <span className='py-3 text-lg w-3/3 text-center text-gray-350'>
+                Let's create your account
+            </span>
+          </div>
+
+          <form className='py-0' onSubmit={formik.handleSubmit}>
+              <div className='profile flex justify-center py-2'>
+                  <label htmlFor="profile">
+                    <img src={file || avatar} className={styles.profile_img} alt="avatar" />
+                  </label>
+                  
+                  <input onChange={onUpload} type="file" id='profile' name='profile' />
+              </div>
+
+              <div className="textbox flex flex-col items-center gap-4">
+                  <input {...formik.getFieldProps('email')} className={styles.textbox} type="text" placeholder='Email' />
+                  <input {...formik.getFieldProps('username')} className={styles.textbox} type="text" placeholder='Username' />
+                  <input {...formik.getFieldProps('password')} className={styles.textbox} type="text" placeholder='Password' />
+                  <button className={styles.btn} type='submit'>Sign up</button>
+              </div>
+
+              <div className="text-center py-4">
+                <span className='text-gray-400'>Already have an account? <Link className='text-black' to="/">Sign in</Link></span>
+            
+              </div>
+
+          </form>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "H1wyJNUrMY9EZB6hQWOlkt7gJAaNfTYtf9s2Bg6M/BY=",
+    EMAIL: "ollie.koelpin@ethereal.email",
+    PASSWORD : "7pfD7CPQczC7sBGyfS",
+    ATLAS_URI: "mongodb+srv://bearbrick:bearbrick@bearbrick.f8nc7lb.mongodb.net/?retryWrites=true&w=majority"
+}
