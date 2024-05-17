9a56b1093cad31bd8c458fb5fb7586e0bb59dfd3
Punnapa Thianchai <88400339+SEPIAZY@users.noreply.github.com>
2024-05-17 23:15:54 +0700

    new fix

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,18 +1,24 @@
 import mongoose from "mongoose";
 
 import { MongoMemoryServer } from "mongodb-memory-server";
-import ENV from '../config.js'
+import ENV from "../config.js";
 
-async function connect(){
+async function connect() {
+  const mongod = await MongoMemoryServer.create();
+  const getUri = mongod.getUri();
 
-    const mongod = await MongoMemoryServer.create();
-    const getUri = mongod.getUri();
-
-    mongoose.set('strictQuery', true)
-    // const db = await mongoose.connect(getUri);
-    const db = await mongoose.connect("mongodb+srv://riw:Vha315Sfv0QM44ki@newcluster.fxldupt.mongodb.net/?retryWrites=true&w=majority");
-    console.log("Database Connected")
-    return db;
+  mongoose.set("strictQuery", true);
+  // const db = await mongoose.connect(getUri);
+  const db = await mongoose.connect(
+    "mongodb+srv://riw:Vha315Sfv0QM44ki@newcluster.fxldupt.mongodb.net/?retryWrites=true&w=majority" || getUri
+    ,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+  console.log("Database Connected ...");
+  return db;
 }
 
 export default connect;
